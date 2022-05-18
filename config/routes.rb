c98e7653eb41d1870fdbd88ac8cc3ae149b05d85
luis miranda <luispsmiranda.96@gmail.com>
2022-05-18 16:09:59 +0100

    97% of the 2nd done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :create]
+    get 'restaurants/new', to: 'restaurants#new'
+    get 'reviews/new', to: 'reviews#new'
+  end
 end
